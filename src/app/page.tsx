47af779250853929947097026bf4b15a6f62f7cb
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2024-12-30 11:23:21 +0100

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-'use client';
+"use client";
 
 import React from "react";
 import Hero from "@/components/Hero-section";
-import ProductShowcase from './../components/Product-showcase/index';
+import ProductShowcase from "./../components/Product-showcase/index";
 
 export default function Page() {
   return (
     <div className="w-full bg-[#FCFCFC]">
       <div className="bg-[#E8E8E8] pt-24 lg:pt-0">
-        <Hero /> 
+        <Hero />
       </div>
       <div>
-      <ProductShowcase />
+        <ProductShowcase />
       </div>
     </div>
   );
